27068c20f3418fd9c06ed584df803d3f949dc462
Arw0046 <128861661+Arw0046@users.noreply.github.com>
2023-11-17 18:24:16 -0500

    Update AVRGCCStart.cmd

--- AVRGCCStart.cmd
+++ AVRGCCStart.cmd
@@ -3,8 +3,7 @@ set Path=%Path%;C:\"yourpath"\AvoidanceBot\TOOLCHAIN\avr-gnu-toolchain\avr\bin
 set Path=%Path%;C:\"yourpath"\AvoidanceBot\TOOLCHAIN\gnuwin32\bin
 set Path=%Path%;C:\"yourpath"\AvoidanceBot\TOOLCHAIN\avrdude-mingw32
 set Path=%Path%;C:\"yourpath"\AvoidanceBot\TOOLCHAIN\notepad++
-set Path=%Path%;C:\"yourpath"\AvoidanceBot\ROBOT
-
-
+set Path=%Path%;C:\"yourpath"\AvoidanceBot\ROBOT\robot
+set Path=%Path%;C:\"yourpath"\AvoidanceBot\ROBOT\headers
 
 start cmd
